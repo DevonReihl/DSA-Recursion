4b91c53f917e2c1564a26a9ec7af9478f84b935d
Devon Reihl <devon.reihl@gmail.com>
2020-10-26 13:51:10 -0700

    recursion exercises 1-7 part of 8

---
+++ Recursion.js
@@ -0,0 +1,135 @@
+//problem 1
+function countingSheep(num) {
+  if(num ===0) {
+    return console.log("All sheep jumper over the fence");
+  }
+
+  console.log(`${num}, Another sheep jumping over the fence`)
+
+  countingSheep(num - 1);
+}
+
+// countingSheep(5);
+
+//problem 2
+function powerCalculator(int, exp) {
+  if(exp < 0) return 'exponent should be greater then 0';
+  if(exp === 0) return 1;
+
+  return int * powerCalculator(int, exp -1);
+}
+
+// console.log(powerCalculator(10, 3))
+
+
+//problem 3
+function reverseString(str) {
+  if( str === '') return '';
+  
+return reverseString(str.slice(1)) + str[0];
+}
+
+// console.log(reverseString('welcome'));
+
+//problem 4
+function nthTriangleNum(num) {
+  if(num ===1) return 1;
+
+  return num + nthTriangleNum(num - 1);
+}
+
+// console.log(nthTriangleNum(4));
+
+//problem 5
+function stringSplitter(str, splitter, arr=['']) {
+  if (str.length ===0) {
+    return arr;
+  }
+  if (str[0] ===splitter) {
+    arr.push('');
+  }
+  else {
+    arr[arr.length - 1] += str[0];
+  }
+
+  return stringSplitter(str.slice(1), splitter, arr);
+}
+
+// console.log(stringSplitter('10/21/1999', '/'))
+
+//problem 6
+function fib(n, results =['']) {
+  if (n === 1){
+    return [1, 1]
+  }
+  results = fib(n-1);
+  results.push(results[n -1] + results[n -2]);
+  return results
+}
+
+// console.log(fib(7))
+
+//problem 7
+function factorial(i) {
+  if( i == 1) {
+    return 1;
+  }
+  return i * factorial(i - 1);
+}
+
+// console.log(factorial(5))
+
+//problem 8 
+function findWayOut( maze, vert =0, horiz =0, path = '') {
+  if (maze[vert][horiz] === 'e') {
+    return `Path to end: ${path}`;
+  }
+
+
+  const right = maze[vert][horiz +1];
+  if(right && right !== '*'){
+    horiz += 1;
+    path += 'R';
+    return findWayOut(maze, vert, horiz, path);
+  }
+
+  const down = maze[vert +1][horiz];
+  if(down && down !== '*'){
+    vert += 1;
+    path += 'D';
+    return findWayOut(maze, vert, horiz, path);
+  }
+  
+  const left = maze[vert][horiz -1];
+  if(left && left !== '*') {
+    maze[vert][horiz] = '*';
+    horiz -= 1;
+    path += 'L';
+    return findWayOut(maze, vert, horiz, path);
+  }
+
+  // const up = maze[vert][horiz -1];
+  // if(up && up !== '*') {
+  //   maze[vert][horiz] = '*';
+  //   vert -= 1;
+  //   path =+ 'L';
+  //   return findWayOut(maze, vert, horiz, path);
+  // }
+  
+}
+
+let mySmallMaze = [
+  [' ', ' ', ' '],
+  [' ', '*', ' '],
+  [' ', ' ', 'e']
+];
+
+let bigMaze = [
+  [' ', ' ', ' ', '*', ' ', ' ', ' '],
+  ['*', '*', ' ', '*', ' ', '*', ' '],
+  [' ', ' ', ' ', '*', ' ', ' ', ' '],
+  [' ', '*', '*', '*', '*', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+
+console.log(findWayOut(bigMaze))
